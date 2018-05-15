6f68a00060d95aeccfe82be36f4f3c88b978d3b7
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2018-05-15 16:08:46 +0200

    init unit tests for activities and presenters

---
+++ app/src/test/java/com/tsdproject/pokerplanning/splashTest/SplashActivityTest.kt
@@ -0,0 +1,38 @@
+package com.tsdproject.pokerplanning.splashTest
+
+import android.graphics.Typeface
+import android.widget.TextView
+import com.tsdproject.pokerplanning.R
+import com.tsdproject.pokerplanning.splash.SplashActivity
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+
+@RunWith(RobolectricTestRunner::class)
+class SplashActivityTest {
+
+    lateinit var splashActivity: SplashActivity
+
+    @Before
+    fun initData() {
+        splashActivity = Robolectric.setupActivity(SplashActivity::class.java)
+    }
+
+    @Test
+    fun shouldNotBeNull() {
+        assertNotNull(splashActivity)
+    }
+
+    @Test
+    fun shouldHaveRegularFont() {
+        val appNameTextView = splashActivity.findViewById<TextView>(R.id.appNameTextView)
+        val typeface = Typeface.createFromAsset(splashActivity.assets, "fonts/SourceCodePro-Regular.ttf")
+        assertEquals(appNameTextView.typeface, typeface)
+    }
+
+}
